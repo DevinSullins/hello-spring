9134d9753aeff2c3a88465f714736850812242cb
Devin Sullins <devinsullins@gmail.com>
2021-02-17 19:43:23 -0600

    an hour wasted on == vs .equals() means I am not doing the bonus. Exercise finished

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("hello")
+@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
 @ResponseBody
 public class HelloController {
+
+
 
     // Handles request at path /hello
     @GetMapping("")
@@ -21,15 +23,35 @@ public class HelloController {
     }
 
     // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello," + name + "!";
+    @RequestMapping("greeting")
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language) {
+        String message = createMessage(name, language);
+        return message;
     }
 
     //Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    @GetMapping("/{name}/{language}")
+    public String helloWithPathParam(@PathVariable String name, @PathVariable String language) {
+        return createMessage(name, language);
+    }
+
+
+    public static String createMessage(String name, String language){
+
+        if (language.equals("sp")){
+            return "Hola, " + name + "!";
+        } else if (language.equals("fr")){
+            return "Bonjour " + name + "!";
+        } else if (language.equals("ru")){
+            return "Zdravstvuyte " + name + "!";
+        } else if (language.equals("jp")){
+            return "Konnichiwa " + name + "!";
+        } else if (language.equals("en")){
+            return "Hello, " + name + "!";
+        } else {
+            return language;
+        }
+
     }
 
     //lives at /hello/form
@@ -37,11 +59,20 @@ public class HelloController {
     public String helloForm(){
         return "<html>" +
                 "<body>" +
-                "<form action='hello' method='post'>" + //submit a request to hello
+                "<form action='greeting' method='post'>" + //submit a request to hello
                 "<input type='text' name='name'>" +
+                "<select name='language' id='language-select'>" +
+                "<option value=''> Language </option>" +
+                "<option value='en'> English </option>" +
+                "<option value='sp'> Spanish </option>" +
+                "<option value='fr'> French </option>" +
+                "<option value='ru'> Russian </option>" +
+                "<option value='jp'> Japanese </option>" +
+                "</select>" +
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
+
 }
